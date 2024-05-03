d5a6c6600dbf1e41b0a58365fab1a9da0ffb1165
Traspa1112 <traspa@frba.utn.edu.ar>
2024-05-02 22:40:27 -0300

    Fixeo y agrego tests

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -4,22 +4,39 @@ import dds.monedero.exceptions.MaximaCantidadDepositosException;
 import dds.monedero.exceptions.MaximoExtraccionDiarioException;
 import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MonederoTest {
   private Cuenta cuenta;
+  private Cuenta cuentaCon500Pesos;
 
   @BeforeEach
   void init() {
     cuenta = new Cuenta();
+    cuentaCon500Pesos = new Cuenta(500, new ArrayList<>());
+  }
+
+  @Test
+  void Saldo500Pesos() {
+    assertEquals(500, cuentaCon500Pesos.getSaldo());
+  }
+
+  @Test
+  void SaldoSinMovimientos() {
+    assertEquals(0, cuenta.getSaldo());
   }
 
   @Test
   void Poner() {
     cuenta.poner(1500);
+    // Fixeo el test vacío
+    assertEquals(1500, cuenta.getSaldo());
   }
 
   @Test
@@ -32,10 +49,12 @@ public class MonederoTest {
     cuenta.poner(1500);
     cuenta.poner(456);
     cuenta.poner(1900);
+    // Fixeo el test vacío
+    assertEquals(3856, cuenta.getSaldo());
   }
 
   @Test
-  void MasDeTresDepositos() {
+  void RealizarMasDe3Depositos() {
     assertThrows(MaximaCantidadDepositosException.class, () -> {
           cuenta.poner(1500);
           cuenta.poner(456);
